388ab5771434483e7857cd2afb7c8003710e760d
RiccardoCampitelli <riccardo.campitelli91@gmail.com>
2019-07-19 14:24:25 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+
+
+import 'semantic-ui-css/semantic.min.css';
+import { Container } from 'semantic-ui-react'
+
+
+import MenuBar from './components/MenuBar';
+import Home  from './pages/Home';
+import Login  from './pages/Login';
+import Register  from './pages/Register';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Container>
+      <MenuBar/>
+      <Route exact path='/' component={Home}/>
+      <Route exact path='/login' component={Login}/>
+      <Route exact path='/register' component={Register}/>
+      </Container>
+    </Router>
   );
 }
 
